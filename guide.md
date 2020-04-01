66b3efeb1c816c9598ec91d3d273a4d17e2d413d
Ferdinand Gumø Løberg <ferdil17@student.uia.no>
2020-04-01 21:57:52 +0200

    Deleted src2, added markdown to html conversion and start of guide page

---
+++ guide.md
@@ -0,0 +1,5 @@
+# Here comes a guide, dudududu
+
+This is a test
+1. here is more
+2. another part of testing
--- src/objects/boxes.ts
+++ src/objects/boxes.ts
@@ -1,5 +1,4 @@
 import { Base } from './conveyors';
-import { Level } from '../types';
 import { Board } from './board';
 
 class BoxHandler {
